8e66c58a298dbb1c17c263ec602d9317d783463b
Will Keller <wkeller3@binghamton.edu>
2019-09-09 10:31:07 -0400

    Added MySurface.java file

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
---
+++ app/src/main/java/com/example/project2/MySurface.java
@@ -0,0 +1,14 @@
+package com.example.project2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.PixelFormat;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
+
+}
