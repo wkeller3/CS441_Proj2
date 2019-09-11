d6f3b6a3a44acddcd46e0845c9e5e31a72fc241c
Will Keller <wkeller3@binghamton.edu>
2019-09-11 10:36:36 -0400

    Added a surface and trying to implement methods drawCircle, Square, and Triangle

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -15,11 +15,72 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
+
+    private Button circle = null;//findViewById(R.id.circle);
+    private Button square = null;//findViewById(R.id.square);
+    private Button triangle = null;//findViewById(R.id.triangle);
+    private int flag; //0 circle, 1 square, 2 triangle
+    private LinearLayout canvasLayout = findViewById(R.id.customViewLayout);
+    MySurface surfaceView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        circle = findViewById(R.id.circle);
+        square = findViewById(R.id.square);
+        triangle = findViewById(R.id.triangle);
+
+        // Hide the app title bar.
+        getSupportActionBar().hide();
+
+        // Make app full screen to hide top status bar.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        surfaceView = new MySurface(getApplicationContext());
+        surfaceView.setOnTouchListener(this);
+        canvasLayout.addView(surfaceView);
+
+
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag = 0;
+            }
+        });
+
+        square.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag = 1;
+            }
+        });
+
+        triangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag = 2;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent){
+        if(view instanceof SurfaceView){
+            view.invalidate();
+
+            if(0 == flag){
+                surfaceView.drawCircle();
+            } else if(1 == flag){
+                surfaceView.drawSqaure();
+            } else{
+                surfaceView.drawTriangle();
+            }
+            return true;
+        } else{
+            return false;
+        }
     }
 
 }
--- app/src/main/java/com/example/project2/MySurface.java
+++ app/src/main/java/com/example/project2/MySurface.java
@@ -5,10 +5,79 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
 public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder surfaceHolder = null;
+    private  Paint paint = null;
+
+    public MySurface(Context context){
+        super(context);
+        surfaceHolder = getHolder();
+        paint = new Paint();
+        paint.setColor(Color.CYAN);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder){
+        drawCircle();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        paint = null;
+
+    }
+
+    public void drawCircle(){
+        surfaceHolder = getHolder();
+        Canvas canvas = surfaceHolder.lockCanvas();
+        Paint background = new Paint();
+        background.setColor(Color.LTGRAY);
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), background);
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(50, 50, 100, paint);
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    public void drawSqaure(){
+        Canvas canvas = surfaceHolder.lockCanvas();
+        Paint background = new Paint();
+        background.setColor(Color.BLACK);
+        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), background);
+        canvas.drawRect(0, 0, 200, 200, paint);
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+    public void drawTriangle(){
+        Canvas canvas = surfaceHolder.lockCanvas();
+        Paint background = new Paint();
+        background.setColor(Color.WHITE);
+
+        Point a = new Point(40,40);
+        Point b = new Point(55,55);
+        Point c = new Point(25,25);
+
+        Path path = new Path();
+        path.lineTo(a.x, a.y);
+        path.lineTo(b.x, b.y);
+        path.lineTo(c.x, c.y);
+        path.close();
+
+        canvas.drawPath(path, paint);
+
+        surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
 
 }
