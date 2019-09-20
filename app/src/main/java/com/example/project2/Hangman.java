dfd2edb129609063682d1e166db581438aad0078
Will Keller <wkeller3@binghamton.edu>
2019-09-20 10:40:03 -0400

    Can now switch between the two activities

--- app/src/main/java/com/example/project2/Hangman.java
+++ app/src/main/java/com/example/project2/Hangman.java
@@ -2,10 +2,15 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Hangman extends AppCompatActivity {
+
+    private Button change = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,14 @@ public class Hangman extends AppCompatActivity {
 
         // Make app full screen to hide top status bar.
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        change = findViewById(R.id.change);
+
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Hangman.this, MainActivity.class));
+            }
+        });
     }
 }
